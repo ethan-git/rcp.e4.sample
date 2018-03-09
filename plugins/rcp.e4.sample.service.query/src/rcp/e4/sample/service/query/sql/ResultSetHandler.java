e9fa53695629586ceccbeb931b5f8fa0d6436b06
ethan <eungjun01@naver.com>
2018-03-09 13:57:25 +0900

    add sql service

---
+++ plugins/rcp.e4.sample.service.query/src/rcp/e4/sample/service/query/sql/ResultSetHandler.java
@@ -0,0 +1,150 @@
+/**
+  * Copyright 2017 ethan.park
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
+package rcp.e4.sample.service.query.sql;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import rcp.e4.sample.core.exception.SystemException;
+
+/**
+ * ResultSetHandler
+ *
+ * @author ejpark
+ *
+ */
+public class ResultSetHandler {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ResultSetHandler.class);
+
+	private Connection connection;
+	private Statement statement;
+	private ResultSet resultSet;
+	private int currentRowIndex;
+	private int totalRowCnt;
+	private int columnCnt;
+	private List<ResultColumnMetaInfo> resultColumnMetaInfoList;
+
+	/**
+	 * ResultSetHandler
+	 *
+	 * @param connection
+	 * @param statement
+	 * @param resultSet
+	 */
+	public ResultSetHandler(Connection connection, Statement statement, ResultSet resultSet) {
+		this.connection = connection;
+		this.statement = statement;
+		this.resultSet = resultSet;
+		this.currentRowIndex = 0;
+		initialize();
+	}
+
+	private void initialize() {
+		try {
+			this.resultColumnMetaInfoList = new ArrayList<ResultColumnMetaInfo>();
+			ResultSetMetaData resultSetMetaData = this.resultSet.getMetaData();
+			this.columnCnt = resultSetMetaData.getColumnCount();
+			for (int i = 0; i < this.columnCnt; i++) {
+				ResultColumnMetaInfo resultColumnMetaInfo = new ResultColumnMetaInfo();
+				resultColumnMetaInfo.setLabel(resultSetMetaData.getColumnLabel(i + 1));
+				this.resultColumnMetaInfoList.add(resultColumnMetaInfo);
+			}
+			this.resultSet.last();
+			this.totalRowCnt = this.resultSet.getRow();
+			this.resultSet.beforeFirst();
+			LOGGER.debug("columnCnt:{}, totalRowCnt:{}", this.columnCnt, this.totalRowCnt);
+		} catch (SQLException e) {
+			throw new SystemException(e);
+		}
+	}
+
+	/**
+	 * getFetchRows
+	 *
+	 * @param fetchSize
+	 * @return rows
+	 */
+	public List<Object[]> getFetchRows(int fetchSize) {
+		List<Object[]> rows = new ArrayList<Object[]>();
+
+		// all rows fetched.
+		if (this.currentRowIndex == this.totalRowCnt) {
+			LOGGER.debug("all rows fetched.[currentRowIndex:{}, totalRowCnt:{}]", this.currentRowIndex,
+					this.totalRowCnt);
+			return rows;
+		}
+
+		try {
+			// set position current row index.
+			this.resultSet.absolute(this.currentRowIndex);
+			this.resultSet.setFetchSize(fetchSize);
+
+			while (rows.size() < fetchSize && this.resultSet.next()) {
+				this.currentRowIndex++;
+				Object[] row = new Object[this.columnCnt];
+				for (int i = 0; i < this.columnCnt; i++) {
+					row[i] = this.resultSet.getObject(i + 1);
+				}
+				rows.add(row);
+			}
+
+			// all rows fetched.
+			if (this.currentRowIndex == this.totalRowCnt) {
+				closeQuietly();
+			}
+		} catch (SQLException e) {
+			throw new SystemException(e);
+		}
+		return rows;
+	}
+
+	/**
+	 * getResultColumnMetaInfoList
+	 *
+	 * @return resultColumnMetaInfoList
+	 */
+	public List<ResultColumnMetaInfo> getResultColumnMetaInfoList() {
+		return this.resultColumnMetaInfoList;
+	}
+
+	/**
+	 * closeQuietly
+	 */
+	public void closeQuietly() {
+		LOGGER.debug("closeQuietly.");
+		try {
+			if (this.statement != null) {
+				this.statement.close();
+			}
+			if (this.connection != null) {
+				this.connection.close();
+			}
+		} catch (SQLException e) {
+			// Ignore.
+			LOGGER.debug("closeQuietly.[Error:{}]", e.getMessage());
+		}
+	}
+}
--- plugins/rcp.e4.sample.service.query/src/rcp/e4/sample/service/query/sql/SqlExecuteService.java
+++ plugins/rcp.e4.sample.service.query/src/rcp/e4/sample/service/query/sql/SqlExecuteService.java
@@ -16,12 +16,58 @@
 
 package rcp.e4.sample.service.query.sql;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.concurrent.Callable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import rcp.e4.sample.connection.pool.ConnectionPoolFactory;
+import rcp.e4.sample.connection.pool.IConnectionPool;
+
 /**
  * SqlExecuteService
  *
  * @author ejpark
  *
  */
-public class SqlExecuteService {
+public class SqlExecuteService implements Callable<ResultSetHandler> {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(SqlExecuteService.class);
+
+	private IConnectionPool connectionPool;
+	private String sql;
+
+	/**
+	 * SqlExecuteService
+	 *
+	 */
+	public SqlExecuteService() {
+		this.connectionPool = ConnectionPoolFactory.getInstance().getConnectionPool();
+	}
+
+	/**
+	 * @param sql
+	 */
+	public void setSql(String sql) {
+		this.sql = sql;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.util.concurrent.Callable#call()
+	 */
+	@Override
+	public ResultSetHandler call() throws Exception {
+		LOGGER.debug("sql:{}", sql);
+		Connection connection = this.connectionPool.getConnection();
+		PreparedStatement preparedStatement = connection.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE,
+				ResultSet.CONCUR_UPDATABLE);
+		ResultSet resultSet = preparedStatement.executeQuery();
+		ResultSetHandler resultSetHandler = new ResultSetHandler(connection, preparedStatement, resultSet);
+		return resultSetHandler;
+	}
 }
